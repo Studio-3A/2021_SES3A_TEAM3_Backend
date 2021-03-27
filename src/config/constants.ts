a3fbae8de70c10b127e07ad3b8d7aada66f099c0
MitchellUTS <13248202@student.uts.edu.au>
2021-03-28 02:21:08 +1100

    Added a example on how to implement database connections and basic CRUD API actions

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,9 @@
 export const PORT = process.env.PORT || 4000;
 export const HOST = process.env.HOST || "localhost";
 export const PROTOCOL = process.env.PROTOCOL || "http";
+
+export const MONGO = {
+    HOST: process.env.MONGO_HOST || "localhost",
+    USERNAME: process.env.MONGO_USERNAME || "user_name",
+    PASSWORD: process.env.MONGO_PASSWORD || "password",
+};

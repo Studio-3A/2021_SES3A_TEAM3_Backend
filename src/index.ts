296ed242ecddb7e2915a817ec63ac8ead2c780fd
tanmaymunigala <tanmay.munigala@gmail.com>
2021-03-29 15:35:57 +1100

    Add neo4j instance connection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import express from "express";
 import cors from "cors";
+import logger from "morgan";
+import path from "path";
 import { PORT, HOST, PROTOCOL } from "./config/constants";
 import { authRouter, userRouter } from "./routes";
+import neo4j from "neo4j-driver";
 
 const app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+
 // Disable annoying browser security
 app.use(cors({ origin: `http://${HOST}:3000` }));
 
 // Use JSON Parsing Middleware
 app.use(express.json());
+
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('testUsername', 'testPassword'));
+var session = driver.session();
 
 // Define a route handler for the default home page (note: probably dont need this)
 app.get("/", (req, res) => res.send("Hello world!"));

4a612380416b3cc7919580c9f0f800405be86357
MitchellUTS <13248202@student.uts.edu.au>
2021-04-13 14:47:12 +1000

    Added persistent sessions (using cookies)

--- src/authentication.ts
+++ src/authentication.ts
@@ -4,20 +4,32 @@ import FirebaseAdmin, { ServiceAccount } from "firebase-admin";
 import serviceAccount from "./config/serviceAccountKey.json";
 import { Request, Response } from "./common/expresstypes";
 
+const cookieOptions = {
+    maxAge: 60 * 60 * 1000, /* 1 hour */
+    httpOnly: true,
+    path: "/",
+    sameSite: false,
+};
+
 // Use as middleware to set the user where based on the header
 export async function parseAuthSession(req: Request, res: Response, next: Function) {
     // TODO: Pass the ID/Token as a header on requests instead of in the body as JSON 
-    const token: string | undefined = req?.header("Firebase-Token");
+    const token: string | undefined = req?.header("Firebase-Token") || req.cookies.FirebaseToken;
 
     if (token) {
         try {
             const decodedToken = await FirebaseAdmin.auth().verifyIdToken(token);
+            if (!req.cookies.FirebaseToken) {
+                res.cookie("FirebaseToken", token, cookieOptions);
+            }
             req.session = decodedToken;
         } catch (err) {
-            req.session = undefined;
+            res.cookie("FirebaseToken", undefined, { ...cookieOptions, maxAge: 0 });
+            req.session = null;
         }
     } else {
-        req.session = undefined;
+        res.cookie("FirebaseToken", undefined, { ...cookieOptions, maxAge: 0 });
+        req.session = null;
     }
     next();
 };

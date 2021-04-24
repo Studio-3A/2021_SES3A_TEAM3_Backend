cc24a3acccbc761b7076066472f97dd9f4e87fc9
MitchellUTS <13248202@student.uts.edu.au>
2021-04-24 21:20:08 +1000

    Move to MySQL

---
+++ prisma/migrations/20210418093719_initial_schema/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Trip` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` VARCHAR(255) NOT NULL,
+UNIQUE INDEX `Trip.userId_unique`(`userId`),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TripLeg` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tripId` INTEGER NOT NULL,
+    `beginDate` DATETIME(3),
+    `endDate` DATETIME(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Trip` ADD FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `TripLeg` ADD FOREIGN KEY (`tripId`) REFERENCES `Trip`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;

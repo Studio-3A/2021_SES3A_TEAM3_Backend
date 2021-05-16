87d81c0107350fe785855a3dd247cbe513179ce9
justinsaji <justinsaji1@gmail.com>
2021-05-16 20:58:53 +1000

    Trips now properly incorporate overnight stays, also made time management more consistent by making everying into ms, and fixed some other bugs here and there - also disabled redis caching temporarily

--- src/routes/Trip/Trip.ts
+++ src/routes/Trip/Trip.ts
@@ -8,9 +8,14 @@ import {
 } from 'travelogue-utility';
 import { getDirections } from '../../apiFetchers/directions';
 
+// timezone stuff
+import getTimezone from "tz-lookup";
+import momentTz from 'moment-timezone';
+
 //Redis Server
 import redis from 'redis';
-export const redis_client = redis.createClient(6379)
+
+let redis_client: redis.RedisClient | undefined; //redis.createClient(6379)
 
 export const tripRouter = express.Router({
   strict: true,
@@ -19,93 +24,106 @@ export const tripRouter = express.Router({
 tripRouter.post('/new', async (req: Request<unknown, unknown, TripGenerationInputs, unknown>, res: Response) => {
   try {
     const input = req.body;
-    CheckInputIsValid(input);
-    const { startLocation, endLocation, startDate, endDate } = input;
+    checkInputIsValid(input);
+    const { startLocation, endLocation } = input;
 
     // Initial example key
     const key = `(${startLocation.lat},${startLocation.lng}), (${endLocation.lat},${endLocation.lng})`;
 
     // At the moment just a direct check
     // TODO: Allow for "similar trips" to be retrievable via cache
-    redis_client.get(key, async (err, result) => {
-      if (err == null && result != null) {
-        return res.send(result);
-      } else {
-        let radius = distanceBetweenTwoCoordinates(startLocation, endLocation) * 0.75;
-
-        // TODO: If radius is more than 25000m, split the journey up instead of using a centre
-        radius = radius > 25000 ? 25000 : radius;
-        // not sure if 15000m should be the min radius heh
-        radius = radius < 15000 ? 15000 : radius;
-
-        const centre = getMidpointBetweenTwoCoordinates(startLocation, endLocation);
-        const placesInput: NearbyPlacesInput = { ...centre, radius, queryByPrice: false };
-        let allPlaces: Place[] = [];
-
-        const getPlaces = async (queryByPrice: boolean, pagetoken?: string, iteration = 1) => {
-          if (iteration > 4) return;
-          let places = await getPlacesByLocation({ ...placesInput, queryByPrice, pagetoken });
-          if (!isErrorResponse(places)) {
-            allPlaces = allPlaces.concat(places.results);
-            if (places.next_page_token) {
-              await getPlaces(queryByPrice, places.next_page_token, iteration + 1);
-            }
-          }
+    if (redis_client) {
+      redis_client.get(key, async (err, result) => {
+        if (err == null && result != null) {
+          return res.send(result);
+        } else {
+          const trip = await constructTrip(input);
+          // Save trip into redis cache
+          redis_client?.set(key, JSON.stringify(trip))
+          return res.send(trip);
         }
+      })
+    } else {
+      const trip = await constructTrip(input);
+      res.send(trip);
+    }
 
-        await getPlaces(true);
-        await getPlaces(false);
-
-        const actual = getRefinedPlaces(allPlaces);
-        const [activities, waypoints] = generateTrip(actual, startDate, endDate);
-        let directions: DirectionsResponse[] = [];
-
-        // start the first request from our actual start
-        let origin: Coordinate | string = startLocation;
-
-        // if there are more than 23 waypoints (excluding start and dest)
-        // we'll have to deal with them 23 at a time - or just use their length if we're not over the max
-        let spliceLen = waypoints.length > 23 ? 23 : waypoints.length;
-
-        // end the first request from the last possible waypoint if we're over the max
-        // or just use the actual end location
-        let destination: Coordinate | string = waypoints.length > 23 ? waypoints[spliceLen] : endLocation;
-
-        while (waypoints.length > 0) {
-          const response = await getDirections({
-            origin, destination, travelModes: ["driving", "walking"],
-            waypoints: waypoints.splice(0, spliceLen)
-          });
-          // if anything happens to go wrong, just don't do anything I guess
-          if (isErrorResponse(response)) { break; }
-
-          directions.push(response)
-          // make the new start location the old end location
-          origin = destination;
-          if (waypoints.length < 23) {
-            // if there are less than 23 waypoints, use whatever we have left
-            // and make sure the end location is actual end location
-            spliceLen = waypoints.length;
-            destination = endLocation;
-          } else {
-            destination = waypoints[spliceLen];
-          }
-        }
-
-        const trip: Trip = { trip: activities, tripId: v4(), directions };
-
-        // Save trip into redis cache
-        redis_client.set(key, JSON.stringify(trip))
-
-        return res.send(trip);
-      }
-    })
   } catch (e) {
     res.send(handleError(e));
   }
 });
 
-const CheckInputIsValid = (input: TripGenerationInputs) => {
+const constructTrip = async (input: TripGenerationInputs): Promise<Trip> => {
+  const { startLocation, endLocation, startDate, endDate } = input;
+
+  let radius = distanceBetweenTwoCoordinates(startLocation, endLocation) * 0.75;
+
+  // TODO: If radius is more than 25000m, split the journey up instead of using a centre
+  radius = radius > 25000 ? 25000 : radius;
+  // not sure if 15000m should be the min radius heh
+  radius = radius < 15000 ? 15000 : radius;
+
+  const centre = getMidpointBetweenTwoCoordinates(startLocation, endLocation);
+  const placesInput: NearbyPlacesInput = { ...centre, radius, queryByPrice: false };
+  let allPlaces: Place[] = [];
+
+  const getPlaces = async (queryByPrice: boolean, pagetoken?: string, iteration = 1) => {
+    if (iteration > 4) return;
+    let places = await getPlacesByLocation({ ...placesInput, queryByPrice, pagetoken });
+    if (!isErrorResponse(places)) {
+      allPlaces = allPlaces.concat(places.results);
+      if (places.next_page_token) {
+        await getPlaces(queryByPrice, places.next_page_token, iteration + 1);
+      }
+    }
+  }
+
+  await getPlaces(true);
+  await getPlaces(false);
+
+  const actual: RefinedPlaces = getRefinedPlaces(allPlaces);
+
+  const initialTimeZone = getTimezone(startLocation.lat, startLocation.lng);
+  const [activities, waypoints] = generateTrip(actual, startDate, endDate, initialTimeZone);
+
+  let directions: DirectionsResponse[] = [];
+
+  // start the first request from our actual start
+  let origin: Coordinate | string = startLocation;
+
+  // if there are more than 23 waypoints (excluding start and dest)
+  // we'll have to deal with them 23 at a time - or just use their length if we're not over the max
+  let spliceLen = waypoints.length > 23 ? 23 : waypoints.length;
+
+  // end the first request from the last possible waypoint if we're over the max
+  // or just use the actual end location
+  let destination: Coordinate | string = waypoints.length > 23 ? waypoints[spliceLen] : endLocation;
+
+  while (waypoints.length > 0) {
+    const response = await getDirections({
+      origin, destination, travelModes: ["driving"],
+      waypoints: waypoints.splice(0, spliceLen)
+    });
+    // if anything happens to go wrong, just don't do anything I guess
+    if (isErrorResponse(response)) { break; }
+
+    directions.push(response)
+    // make the new start location the old end location
+    origin = destination;
+    if (waypoints.length < 23) {
+      // if there are less than 23 waypoints, use whatever we have left
+      // and make sure the end location is actual end location
+      spliceLen = waypoints.length;
+      destination = endLocation;
+    } else {
+      destination = waypoints[spliceLen];
+    }
+  }
+
+  return { trip: activities, tripId: v4(), directions };
+}
+
+const checkInputIsValid = (input: TripGenerationInputs): void => {
   const { startLocation, endLocation, startDate, endDate } = input;
   if (!coordinatesAreValid(startLocation) || !coordinatesAreValid(endLocation)) {
     throw new StatusCodeError(StatusCode.BadRequest, "Start location and end location must be defined");
@@ -114,13 +132,15 @@ const CheckInputIsValid = (input: TripGenerationInputs) => {
   }
 }
 
-const findClosestPlace = (places: Place[], currentPlace: Place | undefined, map: Map<string, number>): { success: boolean, place: Place | undefined, updatedPlaces: Place[] } => {
+const findClosestPlace = (
+  places: Place[], currentPlace: Place | undefined, map: Map<string, number>
+): { place: Place | undefined, updatedPlaces: Place[] } => {
   let minDistance = Infinity;
 
   // if the provided current place was undefined, just choose the first one
   if (currentPlace == null && places.length > 0) {
     places.splice(0, 1);
-    return { success: true, place: places[0], updatedPlaces: places };
+    return { place: places[0], updatedPlaces: places };
   }
 
   // try find the place closest to the current place
@@ -142,17 +162,27 @@ const findClosestPlace = (places: Place[], currentPlace: Place | undefined, map:
     places.splice(minIndex, 1);
   }
 
-  return { success, place: newPlace, updatedPlaces: [...places] };
+  // if there's only 1 place left and it's the current location (almost impossible), remove it :)
+  if (places.length === 1 && places[0].place_id === currentPlace?.place_id) {
+    // TODO: fix the bug that leads to this condition
+    places = [];
+  }
+
+  return { place: newPlace, updatedPlaces: [...places] };
 }
 
 const tryGetDistance = (place1: Place, place2: Place, map: Map<string, number>): number | undefined => {
   return map.get([place1.place_id, place2.place_id].sort().join("-"));
 }
 
-const generateTrip = (places: RefinedPlaces, start: number, end: number): [Activity[], string[]] => {
+/**The number of milliseconds in an hour */
+const msInAnHour = 60 * 60 * 1000;
+
+const generateTrip = (places: RefinedPlaces, start: number, end: number, initialTimeZone: string): [Activity[], string[]] => {
   // do some heavy calcs so we know the distance between each location
   const leastDistanceMap = new Map<string, number>();
-  const allplaces = [...places.nonFood, ...places.food];
+  const allplaces = [...places.nonFood, ...places.food, ...places.lodging];
+
   for (const place1 of allplaces) {
     for (const place2 of allplaces) {
       let result = tryGetDistance(place1, place2, leastDistanceMap);
@@ -164,60 +194,72 @@ const generateTrip = (places: RefinedPlaces, start: number, end: number): [Activ
     }
   }
 
-  const addedPlaces = new Set<string>();
-  const placeIds: string[] = [];
-  const minDuration = 30;
-  let maxDuration = end - start > 180 ? 180 : end - start;
+  const placeIds = new Set<string>();
+  /** The min duration of an activity in ms */
+  const minDuration = 0.5 * msInAnHour;
+  /** The max duration of an activity in ms */
+  const maxDuration = 3 * msInAnHour;
   const activities: Activity[] = [];
   let time = start;
+  /** The time since the last food place was chosen in ms */
   let timeSinceFood = 0;
+
   let currentPlace: Place | undefined;
   let prevPlace: Place | undefined;
+  let timezone = initialTimeZone;
+  let { food: foodPlaces, nonFood: nonFoodPlaces, lodging: lodgingPlaces } = places;
 
-  let { food: foodPlaces, nonFood: nonFoodPlaces } = places;
-  while (time <= end - maxDuration && (foodPlaces.length > 0 || nonFoodPlaces.length > 0)) {
+  while (end - time >= minDuration && (foodPlaces.length > 0 || nonFoodPlaces.length > 0)) {
     const foodPlacesLeft = foodPlaces.length > 0;
     const nonFoodPlacesLeft = nonFoodPlaces.length > 0;
+    const lodgePlacesLeft = lodgingPlaces.length > 0;
 
-    let currentPlaceDidChange = false;
     if (currentPlace) {
       prevPlace = { ...currentPlace };
+      timezone = getTimezone(currentPlace.geometry.location.lat, currentPlace.geometry.location.lng);
+      currentPlace = undefined;
     }
 
     const randomDuration = Math.ceil(Math.random() * (maxDuration - minDuration) + minDuration);
-    const duration = roundUpToNearest10(randomDuration);
+    let duration = roundMsUpToNearest10Minutes(randomDuration);
     if (duration < 0) {
       break;
     }
 
-    // if it's been a while since a meal, or if there are no nonfood places left, just choose another food place
-    if ((timeSinceFood > 180 || !nonFoodPlacesLeft) && foodPlacesLeft) {
-      const result = findClosestPlace(foodPlaces, currentPlace, leastDistanceMap);
-      if (result.success && result.place != null) {
-        currentPlaceDidChange = true;
-        foodPlaces = [...result.updatedPlaces];
+    // if it's after 8pm or before 7am, lets find a place to crash
+    const moment = momentTz(time).tz(timezone);
+    const hoursPast24 = moment.hours();
+    if ((hoursPast24 > 20 || hoursPast24 < 7) && lodgePlacesLeft) {
+      const result = findClosestPlace(lodgingPlaces, currentPlace, leastDistanceMap);
+      lodgingPlaces = [...result.updatedPlaces];
+      if (result.place) {
         currentPlace = { ...result.place };
-        timeSinceFood = 0;
-      } else if (foodPlaces.length === 1 && foodPlaces[0].place_id === currentPlace?.place_id) {
-        // TODO: fix the bug that leads to this condition
-        foodPlaces = [];
+        const extraHours = hoursPast24 < 12 ? 2 : 24 - hoursPast24;
+        duration = (7 + extraHours) * msInAnHour;
+        timeSinceFood = duration;
       }
-    } else if (nonFoodPlacesLeft) {
-      const result = findClosestPlace(nonFoodPlaces, currentPlace, leastDistanceMap);
-      if (result.success && result.place != null) {
-        currentPlaceDidChange = true;
+    } else {
+      // if it's been a while since a meal, or if there are no nonfood places left, just choose another food place
+      if ((timeSinceFood > 3 * msInAnHour || !nonFoodPlacesLeft) && foodPlacesLeft) {
+        const result = findClosestPlace(foodPlaces, currentPlace, leastDistanceMap);
+        foodPlaces = [...result.updatedPlaces];
+        if (result.place != null) {
+          currentPlace = { ...result.place };
+          timeSinceFood = 0;
+        }
+      } else if (nonFoodPlacesLeft) {
+        const result = findClosestPlace(nonFoodPlaces, currentPlace, leastDistanceMap);
         nonFoodPlaces = [...result.updatedPlaces];
-        currentPlace = { ...result.place };
-        timeSinceFood += duration;
-      } else if (nonFoodPlaces.length === 1 && nonFoodPlaces[0].place_id === currentPlace?.place_id) {
-        // TODO: fix the bug that leads to this condition
-        nonFoodPlaces = [];
+        if (result.place != null) {
+          currentPlace = { ...result.place };
+          timeSinceFood += duration;
+        }
       }
     }
 
-    if (currentPlaceDidChange && currentPlace) {
+    if (currentPlace) {
       // only add the place if we haven't visited it already
-      if (!addedPlaces.has(currentPlace.name)) {
+      if (!placeIds.has(currentPlace.place_id)) {
         const { types, name, rating, price_level, vicinity, place_id } = currentPlace;
         activities.push({
           types, name, duration, time, place_id,
@@ -226,23 +268,22 @@ const generateTrip = (places: RefinedPlaces, start: number, end: number): [Activ
           location: vicinity,
         });
 
-        addedPlaces.add(name)
-        placeIds.push(place_id);
+        placeIds.add(place_id);
         time += duration;
 
         if (currentPlace && prevPlace) {
           const travelDist = tryGetDistance(currentPlace, prevPlace, leastDistanceMap)
             || distanceBetweenTwoCoordinates(currentPlace.geometry.location, prevPlace.geometry.location);
           // assume the time taken to travel just the total distance travelled at 50km/hr
-          time += roundUpToNearest10(Math.ceil(travelDist / 50));
+          const timeTravelling = roundMsUpToNearest10Minutes((travelDist / 50000) * msInAnHour);
+          time += timeTravelling;
         }
       }
     }
-
-    maxDuration = end - time > 180 ? 180 : end - time;
   }
 
-  return [activities, placeIds];
+  return [activities, Array.from(placeIds)];
 }
 
-const roundUpToNearest10 = (num: number) => Math.ceil(num * 0.1) * 10;
+const tenMinutesInMs = 10 * 60 * 1000;
+const roundMsUpToNearest10Minutes = (num: number): number => Math.ceil(num / tenMinutesInMs) * tenMinutesInMs;
